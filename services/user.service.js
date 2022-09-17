44dd6f6602a8e6bc74040cc6bda7e5df917368cb
camroddur9 <ui.crodriguezd@gmail.com>
2022-09-17 14:20:42 -0500

    updated version

--- services/user.service.js
+++ services/user.service.js
@@ -1,14 +1,15 @@
-const boom = require('@hapi/boom');
-
+const { models } = require("../libs/sequelize");
 class UserService {
-  constructor() {}
+  constructor() {
+  }
 
   async create(data) {
     return data;
   }
 
   async find() {
-    return [];
+    const rta = await models.User.findAll()
+    return rta
   }
 
   async findOne(id) {
